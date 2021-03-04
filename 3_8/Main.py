a25450763799e6d31f1c70394a10cf40af0ae97d
Inspiration679 <taraskalaida@gmail.com>
2021-03-04 15:19:46 +0200

    New

--- 3_8/Main.py
+++ 3_8/Main.py
@@ -5,10 +5,13 @@ import operator
 from functools import reduce
 import math
 
+text2 = "{:^30}".format("Виконав") + "\n" + "{:^30}".format("Калайда Тарас") + "\n" + "{:^30}".format(
+    "Студент групи ІВ-93") + "\n" + "{:^30}".format("Варіант 8")
+
 
 def graphic1():
     x, y = make(0, 2, 10, 1)
-    x_int, y1 = make(0, 2, 1000, 1)
+    x_int, y1 = make(0, 2, 100, 1)
     y_int = []
     for i in x_int:
         y_int.append(lagrang(i, x, y))
@@ -31,7 +34,7 @@ def graphic1():
 
 def graphic2():
     x, y = make(0, 2, 10, 0)
-    x_int, y1 = make(0, 2, 1000, 0)
+    x_int, y1 = make(0, 2, 100, 0)
     y_int = []
     for i in x_int:
         y_int.append(lagrang(i, x, y))
@@ -50,18 +53,87 @@ def graphic2():
     ax2.tick_params(axis='x', colors="C2")
     ax2.tick_params(axis='y', colors="C2")
     plt.show()
+
+
 def graphic3():
-    w=[]
-    x, y = make(0, 2, 10, 0)
-    x_int, y1 = make(0, 2, 1000, 0)
+    x, y = make(0, 2, 10, 1)
+    x_int, y1 = make(0, 2, 100, 1)
     y_int = []
-    print(x_int)
     for i in x_int:
         y_int.append(lagrang(i, x, y))
-    for i in range(10):
-        pass
+
+    def f(x):
+        return np.sin(x)
+
+    list_constants_sin = []
+    a = len(x_int) - 1
+    while a >= 0:
+        constant = 0
+        for i in range(len(x) - a):
+            znam = 1
+            for j in range(len(x) - a):
+                if i != j:
+                    znam *= (x[i] - x[j])
+            constant += y[i] / znam
+        list_constants_sin.append(constant)
+        a -= 1
+    pol = y_int
+    y_arg = f(list_constants_sin) - y_int
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, label="1")
+
+    ax.plot(x_int, y_arg, color="C3")
+    ax.set_xlabel("Похибка sin(x)", color="C1")
+    ax.tick_params(axis='x', colors="C1")
+    ax.tick_params(axis='y', colors="C1")
+
+    plt.show()
 
 
+def new_window():
+    window = Toplevel(root)
+    window.title("Інфо")
+    window.geometry("280x180")
+    label3 = Label(window, text=text2, font="Ariel 18", width=20, height=15, bg="yellow").place(x=0,
+                                                                                                     y=-115)
+
+
+def graphic4():
+    x, y = make(0, 2, 10, 0)
+    x_int, y1 = make(0, 2, 100, 0)
+    y_int = []
+
+    for i in x_int:
+        y_int.append(lagrang(i, x, y))
+
+    def f(x):
+        return np.cos(x[i] + pow(np.cos(x[i]), 3))
+
+    list_constants_sin = []
+    a = len(x_int) - 1
+    while a >= 0:
+        constant = 0
+        for i in range(len(x) - a):
+            znam = 1
+            for j in range(len(x) - a):
+                if i != j:
+                    znam *= (x[i] - x[j])
+            constant += y[i] / znam
+        list_constants_sin.append(constant)
+        a -= 1
+    pol = y_int
+    y_arg = f(list_constants_sin) - y_int
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, label="1")
+
+    ax.plot(x_int, y_arg, color="C3")
+    ax.set_xlabel("Похибка sin(x)", color="C1")
+    ax.tick_params(axis='x', colors="C1")
+    ax.tick_params(axis='y', colors="C1")
+
+    plt.show()
 
 
 def make(a, b, n, var):
@@ -89,10 +161,21 @@ def lagrang(x, x_values, y_values):
 
 
 root = Tk()
+root.title("Головне вікно")
 root["bg"] = "yellow"
+root.geometry("850x180")
+menu = Menu(root)
+mainmenu = Menu(menu, tearoff=0)
+menu.add_cascade(label="Меню", menu=mainmenu)
+mainmenu.add_command(label="Інфо", command=new_window)
+root.configure(menu=menu)
 Button(root, text="Інтерполяція функції sin(x)", command=graphic1, font=("Times", 15), width=30).place(x=50,
                                                                                                        y=50)
-Button(root, text="Інтерполяція функції cos(x+cosx^3)", command=graphic3, font=("Times", 15), width=30).place(x=50,
+Button(root, text="Інтерполяція функції cos(x+cosx^3)", command=graphic2, font=("Times", 15), width=30).place(x=50,
                                                                                                               y=100)
+Button(root, text="Похибка sin(x)", command=graphic3, font=("Times", 15), width=30).place(x=450,
+                                                                                          y=50)
+Button(root, text="Похибка cos(x+cosx^3)", command=graphic4, font=("Times", 15), width=30).place(x=450,
+                                                                                                 y=100)
 
 root.mainloop()
